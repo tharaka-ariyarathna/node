1a3050d66a719748b755b54b65688177380b7767
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-06-15 17:42:21 +0530

    Added file to start server

--- App.js
+++ App.js
@@ -1,11 +1,1 @@
-const {readFile, writeFile} = require('fs') ;
-
-/*readFile('./test.txt', 'utf-8', (err,result) => {
-    if(err){
-        console.log(err); 
-        return ;
-    }
-    console.log(result) ;
-})*/
-
-console.log("Works ") ;
+require('./Server') ;
