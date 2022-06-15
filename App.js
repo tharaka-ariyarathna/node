1bb577e0b386769e98a1fe7570dab9b1372f2a43
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-06-15 17:39:16 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,11 @@
+const {readFile, writeFile} = require('fs') ;
+
+/*readFile('./test.txt', 'utf-8', (err,result) => {
+    if(err){
+        console.log(err); 
+        return ;
+    }
+    console.log(result) ;
+})*/
+
+console.log("Works ") ;
