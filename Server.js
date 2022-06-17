4822e83f104bd1391810960289ab66071b07c00d
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-06-17 19:14:21 +0530

    Routing in app

--- Server.js
+++ Server.js
@@ -1,1 +1,20 @@
+const http = require('http') ;
+const Server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.end('Welcome to our home page') ;
+    }
+    if(req.url === '/about'){
+        res.end('This is about our backend server') ;
+    }
+    res.end(
+        `
+            <h1>Oops!</h1>
+            <p>Couldn't find the page</p>
+            <a href='/'>Home  </a>
+        `
+    )
+})
+
+Server.listen(5000) ;
+
 console.log("Working") ;
